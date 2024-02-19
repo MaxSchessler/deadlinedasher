c3bd250fc9395d07b0a99880dd1e7c630d0be473
Max Schessler <schessler.max@gmail.com>
2024-02-18 21:21:16 -0600

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Deadline_Dasher.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
